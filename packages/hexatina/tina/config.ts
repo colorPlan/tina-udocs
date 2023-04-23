77adfa9cafbb3441f4352ffea2693ac7ffb0a415
Valentin Iancu <valentind.iancu@gmail.com>
2023-04-23 16:48:21 -0400

    fixes

--- packages/hexatina/tina/config.ts
+++ packages/hexatina/tina/config.ts
@@ -6,9 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: process.env.TINACMS_CLIENT_ID, // Get this from tina.io
-  token: process.env.TINACMS_TOKEN, // Get this from tina.io
-
+  clientId: "08301358-cbb0-4fb3-9afc-ac63f6588818", // Get this from tina.io
+  token: "a7fc4ccbf98b778c9e41df480febee059bd177d9", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
