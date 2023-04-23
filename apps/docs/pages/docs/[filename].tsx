240e7657a4deb78514feb980599d961dd33306ca
Valentin Iancu <valentind.iancu@gmail.com>
2023-04-23 19:06:04 -0400

    Fixes

--- apps/docs/pages/docs/[filename].tsx
+++ apps/docs/pages/docs/[filename].tsx
@@ -4,7 +4,7 @@
   import Head from 'next/head'
   import { useTina } from 'tinacms/dist/react'
   import { TinaMarkdown } from 'tinacms/dist/rich-text'
-  import client from 'hexatina/tina/__generated__/client'
+  import client from '@tina/__generated__/client'
 
   const BlogPage = (props) => {
     const { data } = useTina({
