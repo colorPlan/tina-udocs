067bc0268092c059ce80fa9da826148f14c27389
Valentin Iancu <valentind.iancu@gmail.com>
2023-04-23 15:41:38 -0400

    Update tina credentials

--- sites/docs_1/tina/config.ts
+++ sites/docs_1/tina/config.ts
@@ -7,9 +7,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
-
+  clientId: "08301358-cbb0-4fb3-9afc-ac63f6588818", // Get this from tina.io
+  token: "a7fc4ccbf98b778c9e41df480febee059bd177d9", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
